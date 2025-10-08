50858da96fb16f68e84d84a1fe0dc7eb259913e8
Nathaniel Josse <nathaniel.josse@gmail.com>
2025-10-08 16:13:44 +0200

    Advanced exercise (not my greatest commit message)

--- etape1/launch.sh
+++ etape1/launch.sh
@@ -1,18 +1,15 @@
 docker pull nginx:1.29.2
 
-docker pull php:latest
+docker pull php:fpm
 
 docker network create reseau
 
 docker container run -d -p 8080:80 -v .:/usr/share/nginx/html --name http --network reseau nginx:1.29.2
 
-docker container run -d -v ./index.php:/app --name script --network reseau php:latest
+docker container run -d -v .:/app --name script --network reseau php:fpm
 
 docker container cp http:/etc/nginx/conf.d/default.conf ./config/default.conf
 
-# Modification du fichier default.conf à la main sur VSCode pour ajouter le support de PHP. On rerun le container http après avoir supprimé l'ancien.
+# Modification du fichier default.conf à la main sur VSCode pour ajouter le support de PHP.
 
-docker container stop http
-docker container rm http
-
-docker container run -d -p 8080:80 -v .:/usr/share/nginx/html --name http --network reseau nginx:1.29.2
+docker container cp ./config/default.conf http:/etc/nginx/conf.d/default.conf
---
+++ etape2/launch.sh
@@ -0,0 +1,15 @@
+docker pull apache:latest
+docker pull php:fpm
+docker pull mariadb:latest
+
+docker network create reseau2
+
+docker container run -d -p 8081:80 -v .:/usr/local/apache2/htdocs/ --name http2 --network reseau2 apache:latest
+
+docker container run -d -v .:/app --name script2 --network reseau2 php:fpm
+
+docker container run -d -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=site -e MYSQL_USER=user -e MYSQL_PASSWORD=password -e MARIADB_RANDOM_ROOT_PASSWORD=yes -v "$(pwd)/create.sql:/docker-entrypoint-initdb.d/create.sql" --name data2 --network reseau2 mariadb:latest
+
+# We add mysqli extension to php-fpm
+docker container exec -it script2 docker-php-ext-install mysqli
+
